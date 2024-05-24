1980bec5370fe913a70797ce6d8fbd1ca238193f
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-05-24 21:16:57 +0200

    buffer image.draw for INSANE GAIN

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod graphics;
 
+use std::io::Write;
 use std::net::TcpStream;
 use std::thread;
 
@@ -10,14 +11,19 @@ fn main() -> std::io::Result<()> {
         s.spawn(loop_stream);
         s.spawn(loop_stream);
         s.spawn(loop_stream);
+        s.spawn(loop_stream);
+        s.spawn(loop_stream);
+        s.spawn(loop_stream);
     });
     Ok(())
 }
 
-fn loop_stream() {
+fn loop_stream() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:1337").unwrap();
     let img = Image::from_bmp("./src/pic.bmp").unwrap();
+    let mut buf = vec![];
+    img.draw(&mut buf).unwrap();
     loop {
-        img.draw(&mut stream).unwrap()
+        stream.write_all(buf.as_slice())?;
     }
 }
