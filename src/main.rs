debdeb310a6c630529ae592b212130f08337c5ab
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-05-24 21:32:52 +0200

    add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,26 @@ use std::io::Write;
 use std::net::TcpStream;
 use std::thread;
 
+use clap::Parser;
+
 use graphics::{prelude::*, Image};
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    threads: u8,
+}
+
 fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+
     thread::scope(|s| {
-        s.spawn(loop_stream);
-        s.spawn(loop_stream);
-        s.spawn(loop_stream);
-        s.spawn(loop_stream);
-        s.spawn(loop_stream);
-        s.spawn(loop_stream);
+        for _ in 0..args.threads {
+            s.spawn(loop_stream);
+        }
     });
     Ok(())
 }
